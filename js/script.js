521866eac55a518433f85a5db7b8e9b576115e57
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-09-10 11:16:23 -0600

    Add Javascript to layout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 var questionOne = document.getElementsByName("question1");
 how = document.getElementById('how');
+stars = document.getElementById('stars');
 blockOne = document.getElementById('blockOne');
 blockTwo = document.getElementById('blockTwo');
+blockThree = document.getElementById('blockThree');
 questionTwo = document.getElementsByName("question2");
-starsRowOne = document.getElementById('starsRowOne');
-starsRowTwo = document.getElementById('starsRowTwo');
-starsToFill = document.getElementsByClassName('star-btn text-dark star-empty');
+starsRow = document.getElementsByClassName('star');
+userName = document.getElementById('userName');
+work = document.getElementById('work');
+age = document.getElementById('age');
+formButton = document.getElementById('formButton');
 for (let index = 0; index < questionOne.length; index++) {
     questionOne[index].addEventListener("click", stepOne);
 }
@@ -16,7 +20,7 @@ function stepOne(r) {
 }
 function sendValues(response)
 {
-    how.innerHTML = response;
+    how.value = response;
     hiddeStepOne();
 }
 function hiddeStepOne(){
@@ -27,30 +31,38 @@ function showStepTwo(){
     blockTwo.style.display='inline-block';
     blockTwo.setAttribute('class', 'card animated slideInRight');
 }
-for (let index = 0; index < questionTwo.length; index++) {
-    questionTwo[index].addEventListener("click", stepTwo);
-}
-function stepTwo(s)
-{
-    s.preventDefault();
-    var star = s.target.getAttribute('response');
-    replaceStars(star);
-}
-function replaceStars(s){
-    starsRowOne.style.display = "none";
-    starsRowTwo.style.display = "block";
-    alert(s);
-    fillStars(s);
+for (let index = 0; index < starsRow.length; index++) {
+    starsRow[index].addEventListener('click', stepTwo);
 
 }
-function fillStars(s){
-    var btns = [];
-    //console.log(starsToFill);
-    for (let index = 0; index < s; index++) {
-        btns.push(starsToFill[index]);
+function stepTwo(e){
+    e.preventDefault();
+    var starValue = e.target.getAttribute('href');
+    sendStarsValues(starValue);
+}
+function sendStarsValues(starValue){
+    stars.value = starValue;
+    hiddeStepTwo();
+}
+function hiddeStepTwo(){
+    blockTwo.style.display='none';
+    showStepThree();
+}
+function showStepThree(){
+    blockThree.style.display='inline-block';
+    blockThree.setAttribute('class', 'card animated slideInDown');
+}
+function sendData(){
+    if (userName.value!="" && work.value !="" && age.value!="") {
+        return true;
     }
-    for (let index = 0; index < btns.length; index++) {
-        btns[index].removeAttribute('class');
-        btns[index].setAttribute('class', 'star-btn text-warning star-fill');
-    }
+}
+form.addEventListener('submit', showData);
+function showData(){
+    console.log(how);
+    console.log(stars);
+    console.log(userName);
+    console.log(work);
+    console.log(age);
+    alert("Como obtuvo los zapatos: "+how.value+"\nClasificacion del zapato: "+stars.value+"\nNombre: "+userName.value+"\nTrabaja en: "+work.value+"\nDe edad: "+age.value);
 }
