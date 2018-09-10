0b05991062b8cc7e9c16144e4186d2b0672379ca
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-09-10 11:25:07 -0600

    Add comments to JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Elements
+//-----------------------------------------------------------
 var questionOne = document.getElementsByName("question1");
 how = document.getElementById('how');
 stars = document.getElementById('stars');
@@ -10,54 +12,81 @@ userName = document.getElementById('userName');
 work = document.getElementById('work');
 age = document.getElementById('age');
 formButton = document.getElementById('formButton');
+//End Elements
+//Event - click button first question
+//-----------------------------------------------------------
 for (let index = 0; index < questionOne.length; index++) {
     questionOne[index].addEventListener("click", stepOne);
 }
+//Get the value of the button selected
+//-----------------------------------------------------------
 function stepOne(r) {
     r.preventDefault();
     var response = r.target.getAttribute('response');
     sendValues(response);
 }
+//Set value to the how input
+//-----------------------------------------------------------
 function sendValues(response)
 {
     how.value = response;
     hiddeStepOne();
 }
+//Hidde the card of first question
+//-----------------------------------------------------------
 function hiddeStepOne(){
     blockOne.style.display='none';
     showStepTwo();
 }
+//display the second card & add animation
+//-----------------------------------------------------------
 function showStepTwo(){
     blockTwo.style.display='inline-block';
     blockTwo.setAttribute('class', 'card animated slideInRight');
 }
+//Event #2 click on stars
+//-----------------------------------------------------------
 for (let index = 0; index < starsRow.length; index++) {
     starsRow[index].addEventListener('click', stepTwo);
 
 }
+//Get the value of href of the links
+//-----------------------------------------------------------
 function stepTwo(e){
     e.preventDefault();
     var starValue = e.target.getAttribute('href');
     sendStarsValues(starValue);
 }
+//Set the value of stars input
+//-----------------------------------------------------------
 function sendStarsValues(starValue){
     stars.value = starValue;
     hiddeStepTwo();
 }
+//Hidde the second card
+//-----------------------------------------------------------
 function hiddeStepTwo(){
     blockTwo.style.display='none';
     showStepThree();
 }
+//Display the second card & add animation
+//-----------------------------------------------------------
 function showStepThree(){
     blockThree.style.display='inline-block';
     blockThree.setAttribute('class', 'card animated slideInDown');
 }
+//Validate the form
+//-----------------------------------------------------------
 function sendData(){
     if (userName.value!="" && work.value !="" && age.value!="") {
         return true;
     }
 }
+//Third Event submit
+//-----------------------------------------------------------
 form.addEventListener('submit', showData);
+//Show data
+//-----------------------------------------------------------
 function showData(){
     console.log(how);
     console.log(stars);
